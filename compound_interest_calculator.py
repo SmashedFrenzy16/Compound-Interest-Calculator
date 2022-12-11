ad9f2510bf9bffc52745953010289806c40b6df1
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-12-11 09:29:55 +0000

    Add files via upload

---
+++ compound_interest_calculator.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from math import *
+
+root = Tk()
+
+root.title("Compound Interest Calculator By @SmashdFrenzy16")
+
+princ = Entry(root, width=100, borderwidth=5)
+princ.pack()
+princ.insert(0, "Enter Principle Amount")
+rate = Entry(root, width=100, borderwidth=5)
+rate.pack()
+rate.insert(0, "Enter Rate")
+time = Entry(root, width=100, borderwidth=5)
+time.pack()
+time.insert(0, "Enter Time")
+
+def compound_interest():
+
+
+    amount = float(princ.get())*(1+(float(rate.get())/100))**float(time.get())
+
+    ci = amount - float(princ.get())
+
+    cilabel = Label(root, text=ci)
+    cilabel.pack()
+
+enter = Button(root, text="Enter", command=compound_interest)
+enter.pack()
+
+root.mainloop()
